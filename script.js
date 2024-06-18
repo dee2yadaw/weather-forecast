142f4ada6244fbe2f5b63e73347fad6a73b3389c
dee2yadaw <140201030+dee2yadaw@users.noreply.github.com>
2024-06-18 10:53:19 +0530

    api key added
    missed api key now added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // script.js
 async function getWeather() {
     const city = document.getElementById('city').value;
-    const apiKey = 'YOUR_API_KEY'; // Replace with your actual API key
+    const apiKey = '0a7d24af58153a66ac961acbe82e1cd0'; // Replace with your actual API key
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     try {
